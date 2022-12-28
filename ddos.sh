d0bc9de9918624bc5113539c6faf1c85ddab49fd
OVHGERMANY <55898675+OVHGERMANY@users.noreply.github.com>
2022-12-28 08:37:30 -0600

    Update ddos.sh

--- ddos.sh
+++ ddos.sh
@@ -1,7 +1,7 @@
-#tested and coded by ovhgermany @bots.syn
-#also you can edit how you would like just dont steal credits pls
-
 #!/bin/bash
+
+# Flush existing iptables rules
+iptables -F
 
 # Set the maximum number of connections per IP
 MAX_CONNECTIONS=100
@@ -10,10 +10,7 @@ MAX_CONNECTIONS=100
 RESET_TIME=300
 
 # Set the IP address of the user to whitelist
-WHITELISTED_IP=128.0.0.1
-
-# Flush existing iptables rules
-iptables -F
+WHITELISTED_IP=70.118.215.105
 
 # Allow established connections
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
@@ -31,12 +28,25 @@ iptables -A INPUT -p tcp --tcp-flags FIN,ACK FIN,ACK -j DROP
 iptables -A INPUT -p tcp --tcp-flags FIN,ACK,URG FIN -j DROP
 iptables -A INPUT -p tcp --tcp-flags FIN,ACK,URG FIN,ACK,URG -j DROP
 
+# Block UDP traffic that exceeds the maximum number of packets per second
+iptables -A INPUT -p udp -m limit --limit 50/second --limit-burst 100 -j ACCEPT
+
 # Block traffic that exceeds the maximum number of connections per IP
 iptables -A INPUT -p tcp -m connlimit --connlimit-above $MAX_CONNECTIONS -j DROP
 
 # Reset the connection count for an IP after a certain amount of time has passed
 iptables -A INPUT -p tcp -m recent --name conn_limit --rcheck --seconds $RESET_TIME -j ACCEPT
 iptables -A INPUT -p tcp -m recent --name conn_limit --set -j DROP
+
+# Block traffic with invalid TCP flags
+iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+
+# Block traffic with invalid IP options
+iptables -A INPUT -m ipoption --ipopt-type any -j DROP
+
+# Block traffic with invalid TCP options
+iptables -A INPUT -p tcp --tcp-option 10 -j DROP
 
 # Allow all other traffic
 iptables -A INPUT -j ACCEPT
